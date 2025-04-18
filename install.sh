e329501f1977116edb76524ec2f17ee5f3691564
zetaloop <zetaloop@outlook.com>
2025-04-19 03:55:00 +0800

    Update install.sh menu content
    Revised the menu content in install.sh by removing promotional text and author details. Updated the script description to 'Vasma Chaining v3.4.7'.

--- install.sh
+++ install.sh
@@ -10014,16 +10014,9 @@ singBoxVersionManageMenu() {
 menu() {
     cd "$HOME" || exit
     echoContent red "\n=============================================================="
-    echoContent green "作者：mack-a"
-    echoContent green "当前版本：v3.4.7"
-    echoContent green "Github：https://github.com/mack-a/v2ray-agent"
-    echoContent green "描述：八合一共存脚本\c"
+    echoContent green "Vasma Chaining v3.4.7\c"
     showInstallStatus
     checkWgetShowProgress
-    echoContent red "\n=========================== 推广区============================"
-    echoContent red "                                              "
-    echoContent green "VPS选购攻略：https://www.v2ray-agent.com/archives/1679975663984"
-    echoContent green "年付10美金低价VPS AS4837：https://www.v2ray-agent.com/archives/racknerdtao-can-zheng-li-nian-fu-10mei-yuan"
     echoContent red "=============================================================="
     if [[ -n "${coreInstallType}" ]]; then
         echoContent yellow "1.重新安装"
