d534da9cdfcad2f27c0351a6792aaad698a9e39e
zetaloop <zetaloop@outlook.com>
2025-09-29 05:16:59 +0800

    fix: 修复VLESS链式分流遗漏字段

--- install.sh
+++ install.sh
@@ -7873,6 +7873,10 @@ prepareVlessChainOutbound() {
         esac
     done
     sni=${sni:-$host}
+    local userFlowField=""
+    if [[ -n "${flow}" ]]; then
+        userFlowField=", \"flow\": \"${flow}\""
+    fi
     if [[ "${security}" == "tls" && "${flow}" == "xtls-rprx-vision" ]]; then
         proto="vision"
     elif [[ "${security}" == "reality" ]]; then
@@ -7935,7 +7939,7 @@ EOF
         {
             "protocol": "vless",
             "tag": "${vlessChainTag}",
-            "settings": {"vnext": [{"address": "${host}", "port": ${port}, "users": [{"id": "${uuid}", "encryption": "none", "flow": "xtls-rprx-vision"}]}]},
+            "settings": {"vnext": [{"address": "${host}", "port": ${port}, "users": [{"id": "${uuid}", "encryption": "none"${userFlowField}}]}]},
             "streamSettings": {"network": "tcp", "security": "tls", "tlsSettings": {"serverName": "${sni}"}}
         }
     ]
@@ -7948,7 +7952,7 @@ EOF
         {
             "protocol": "vless",
             "tag": "${vlessChainTag}",
-            "settings": {"vnext": [{"address": "${host}", "port": ${port}, "users": [{"id": "${uuid}", "encryption": "none"}]}]},
+            "settings": {"vnext": [{"address": "${host}", "port": ${port}, "users": [{"id": "${uuid}", "encryption": "none"${userFlowField}}]}]},
             "streamSettings": {"network": "tcp", "security": "reality", "realitySettings": {"serverName": "${sni}", "publicKey": "${publicKey}", "shortId": "${shortId}", "fingerprint": "${fp}"}}
         }
     ]
